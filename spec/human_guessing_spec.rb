e9a5ee5beded88dd17a02e4ebc0aab832fc74178
Nicolas Caif <r.nicolascaif@gmail.com>
2019-05-17 22:16:46 -0300

    modified testing acordingly to latest model changes

--- spec/human_guessing_spec.rb
+++ spec/human_guessing_spec.rb
@@ -1,22 +1,29 @@
 require_relative '../lib/human_guessing'
 
 describe HumanGuessing do
-  describe '#guessing' do
-    it "returns 'error' when guess its not a 4 digit number" do
-      newgame = HumanGuessing.new
-
-      actual = newgame.guessing('0234')
-      expected = 'Error, favor de adivinar un numero de 4 digitos'
+  describe '#guessme' do
+    it 'guessme is an integer' do
+      actual = HumanGuessing.new.guessme.join.to_i.integer?
+      expected = true
 
       expect(actual).to eq(expected)
     end
 
-    it "returns '2 good and 2 regular' if there is two exact digit position and 2 other matching digits out of position" do
+    it 'guessme 4 digits' do
+      actual = HumanGuessing.new.guessme.length
+      expected = 4
+
+      expect(actual).to eq(expected)
+    end
+  end
+
+  describe '#guess_vs_guessme' do
+    it "returns '2 good and 2 regular' if 2 maching numer and position and 2 only number" do
       newgame = HumanGuessing.new
       newgame.guessme = [1, 2, 4, 3]
-      newgame.guessing('1234')
+      newgame.guess_vs_guessme('1234')
 
-      actual = newgame.clues
+      actual = [newgame.good, newgame.regular]
       expected = [2, 2]
 
       expect(actual).to eq(expected)
@@ -25,9 +32,9 @@ describe HumanGuessing do
     it "returns '4 good and 0 regular' if guess matches machine number" do
       newgame = HumanGuessing.new
       newgame.guessme = [1, 2, 3, 4]
-      newgame.guessing('1234')
+      newgame.guess_vs_guessme('1234')
 
-      actual = newgame.clues
+      actual = [newgame.good, newgame.regular]
       expected = [4, 0]
 
       expect(actual).to eq(expected)
